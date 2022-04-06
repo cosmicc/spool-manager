be5805b48279d56983560e9a2098182d9238a791
cosmicc <ianperry99@gmail.com>
2022-04-06 17:36:47 -0400

    update

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
+echo -n "Installing gcode shell command script to Klipper extras...\r"
 cp gcode_shell_command.py ~/klipper/klippy/extras
-pip3 install -r requirements.txt
+echo " Complete"
+echo -n "Installing Python requirements..."
+pip3 -q install -r requirements.txt
+echo " Complete"
