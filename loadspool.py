b0f46bf95bd940908a64b409df06dae8660f5521
cosmicc <ianperry99@gmail.com>
2022-04-05 23:32:17 -0400

    logging update

--- loadspool.py
+++ loadspool.py
@@ -7,16 +7,25 @@
 # https://github.com/cosmicc/spool-manager
 #
 
+import sys
 import pandas as pd
 import configparser
 import logging
 
 vars_file = '/home/pi/klipper_config/saved_vars.cfg'
 spool_data = '/home/pi/klipper_config/spools.csv'
+logfile = 'spoolmanager.log'
 
-Log_Format = "%(levelname)s %(asctime)s - %(message)s"
-logging.basicConfig(filename = "spoolmanager.log", filemode = "w", format = Log_Format, level = logging.INFO)
-logger = logging.getLogger()
+console_handler = logging.StreamHandler(sys.stdout)
+file_handler = logging.FileHandler(filename=logfile)
+handlers = [file_handler, console_handler]
+
+logging.basicConfig(
+        handlers=handlers,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.DEBUG)
+
+log = logging.getLogger()
 
 variables = configparser.ConfigParser()
 variables.read(vars_file)
@@ -27,12 +36,12 @@ elif 'loaded_spool' not in variables['Variables']:
     logger.error('No variable loaded_spool in section [Variables]')
     exit(1)
 loaded_spoolcode = variables['Variables']['loaded_spool'].replace("'", "")
-logger.info(f'Current Spool ID read from saved_vars.cfg: {loaded_spoolcode}')
+log.debug(f'Current Spool ID read from saved_vars.cfg: {loaded_spoolcode}')
 
 spooldata = pd.read_csv(spool_data)
 
 spool_count = len(spooldata.index)
-logger.info(f'Loaded {spool_count} spools')
+log.debug(f'Loaded {spool_count} spools')
 
 
 loaded_spooldata = spooldata.loc[spooldata['Spool Code'] == loaded_spoolcode.upper()]
@@ -41,4 +50,9 @@ if len(loaded_spooldata.index) == 0:
     logger.error(f'Spool Code [{loaded_spoolcode}] not found in spool data')
     exit(2)
 
-logger.info(f'Found Spool Data for [{loaded_spoolcode}]')
+log.debug(f'Found Spool Data for [{loaded_spoolcode}]')
+
+loaded_spoolweight = loaded_spooldata['Remaining Weight [g]'].values[0]
+loaded_spoolname = f"{loaded_spooldata['Color'].values[0]} {loaded_spooldata['Material'].values[0]} [{loaded_spoolcode.upper()}]"
+log.info(f'Spool: {loaded_spoolname} Remaining weight: {loaded_spoolweight}g')
+
